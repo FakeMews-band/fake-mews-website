845e161c100440a8344e083fb4c2ddbd71d67b73
Alexandre121298 <alexandre.dubois121298@outlook.fr>
2025-10-20 11:59:59 +0200

    Changement animation entré site + ajout de la smooth animation lien footer

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../style/Hero.css";
 
 export default function Hero() {
+
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setVisible(true), 200); // petit délai pour un effet smooth
+  }, []);
+
   return (
-    <section className="hero">
-      <div className="hero-overlay">
+    <section className={`hero ${visible ? "visible" : ""}`} id="hero">
+      <div className="hero-content">
         <h1 className="hero-title">Fake Mews</h1>
         <p className="hero-subtitle">A Tribute to Muse</p>
         <a href="#shows" className="hero-btn">Voir les concerts</a>
