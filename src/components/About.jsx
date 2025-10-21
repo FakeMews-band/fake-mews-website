cb62ccfa6ec68a0fb74da09eea250583a104a482
Alexandre121298 <alexandre.dubois121298@outlook.fr>
2025-10-22 01:22:02 +0200

    Ajout animation neon entre sections

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,33 +20,33 @@ export default function About() {
   }, []);
 
   return (
-    <section ref={sectionRef} className="about">
-      <h2 className="about-title">À propos du groupe</h2>
-      <p className="about-desc">
-        <strong>Fake Mews</strong> est un groupe tribute dédié à l’univers de
-        <span className="muse-highlight"> Muse</span>.  
-        Passionnés par leur énergie et leur créativité, nous reprenons leurs plus grands titres
-        avec une mise en scène fidèle, des sonorités électrisantes et un jeu de lumière immersif.
-      </p>
+      <section ref={sectionRef} className="about">
+        <h2 className="about-title">À propos du groupe</h2>
+        <p className="about-desc">
+          <strong>Fake Mews</strong> est un groupe tribute dédié à l’univers de
+          <span className="muse-highlight"> Muse</span>.  
+          Passionnés par leur énergie et leur créativité, nous reprenons leurs plus grands titres
+          avec une mise en scène fidèle, des sonorités électrisantes et un jeu de lumière immersif.
+        </p>
 
-      <div className="about-members">
-        <div className="member-card">
-          <h3>Thomas</h3>
-          <p>Chant/Clavier</p>
+        <div className="about-members">
+          <div className="member-card">
+            <h3>Thomas</h3>
+            <p>Chant/Clavier</p>
+          </div>
+          <div className="member-card">
+            <h3>Alexandre</h3>
+            <p>Guitare</p>
+          </div>
+          <div className="member-card">
+            <h3>Isaac</h3>
+            <p>Basse</p>
+          </div>
+          <div className="member-card">
+            <h3>François</h3>
+            <p>Batterie/Choeurs</p>
+          </div>
         </div>
-        <div className="member-card">
-          <h3>Alexandre</h3>
-          <p>Guitare</p>
-        </div>
-        <div className="member-card">
-          <h3>Isaac</h3>
-          <p>Basse</p>
-        </div>
-        <div className="member-card">
-          <h3>François</h3>
-          <p>Batterie/Choeurs</p>
-        </div>
-      </div>
-    </section>
+      </section>
   );
 }
